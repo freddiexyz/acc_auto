7deaf5d40100b2563c9aa4aedcebc67838cad0c3
Freddie <32058398+freddiexyz@users.noreply.github.com>
2017-09-27 13:57:27 +1300

    Update acc_automater.py

--- acc_automater.py
+++ acc_automater.py
@@ -3,8 +3,11 @@ from acc_form_entry import FormEntry
 
 def main():
     with SQLgetter() as opendental, FormEntry() as webdriver:
-        while opendental.get_available():
-            opendental.submit_claim(webdriver.enter_data(opendental.get_procs()))
+        try:
+            while opendental.get_available():
+                opendental.submit_claim(webdriver.enter_data(opendental.get_procs()))
+        except KeyboardInterrupt:
+            pass
             
 
 if __name__ == '__main__':
